7c0fba0237fba9e1b9fff3cf5c3bb16ac53c93c8
MinexAutomation <minex.automation@gmail.com>
2020-02-07 00:04:52 -0800

    Added the ICSharpVisualStudioProjectFileNameConventions-based VisualStudioProjectFileNameConventions implementatio of the IVisualStudioProjectFileNameConventions service.

---
+++ source/R5T.Hladir.CSharp/Code/Services/Implementations/VisualStudioProjectFileNameConventions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+namespace R5T.Hladir.CSharp
+{
+    public class VisualStudioProjectFileNameConventions : IVisualStudioProjectFileNameConventions
+    {
+        private ICSharpVisualStudioProjectFileNameConventions CSharpVisualStudioProjectFileNameConventions { get; }
+
+
+        public VisualStudioProjectFileNameConventions(ICSharpVisualStudioProjectFileNameConventions cSharpVisualStudioProjectFileNameConventions)
+        {
+            this.CSharpVisualStudioProjectFileNameConventions = cSharpVisualStudioProjectFileNameConventions;
+        }
+
+        public string GetProjectFileName(string projectName)
+        {
+            var projectFileName = this.CSharpVisualStudioProjectFileNameConventions.GetCSharpProjectFileName(projectName);
+            return projectFileName;
+        }
+
+        public string GetProjectName(string projectFileName)
+        {
+            var projectName = this.CSharpVisualStudioProjectFileNameConventions.GetCSharpProjectName(projectFileName);
+            return projectName;
+        }
+    }
+}
